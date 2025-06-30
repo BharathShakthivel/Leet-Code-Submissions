da191ef55b108587c83254ee4d5f713f82a8ae6d
Bharath Shakthivel <75258943+BharathShakthivel@users.noreply.github.com>
2025-06-30 19:48:15 +0000

    [LeetCode Sync] Runtime - 16 ms (13.88%), Memory - 17.8 MB (100.00%)

--- Submissions/0009-palindrome-number/solution.py
+++ Submissions/0009-palindrome-number/solution.py
@@ -1,6 +1,16 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        x=str(x)
-        if x == x[::-1]:
+        result = 0
+        if x < 0:
+            return False
+        else:
+         y = x       
+         while y!= 0:
+            digit = y % 10
+            result = result * 10 + digit
+            y = y // 10
+         if result == x:
             return True
-        return False    
+         else:
+            return False      
+
