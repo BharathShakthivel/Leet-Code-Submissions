f3f10d6adcc513569f392656b249a5e4986ba2eb
Bharath Shakthivel <75258943+BharathShakthivel@users.noreply.github.com>
2026-01-12 22:03:29 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 20.6 MB (11.79%)

--- Submissions/0001-two-sum/solution.py
+++ Submissions/0001-two-sum/solution.py
@@ -1,9 +1,21 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        prev_dict = {}
-        for i,n in enumerate(nums):
-            diff = target - n
-            if diff in prev_dict:
-                return [prev_dict[diff],i]
-            prev_dict[n] = i
+    #     prev_dict = {}
+    #     for i,n in enumerate(nums):
+    #         diff = target - n
+    #         if diff in prev_dict:
+    #             return [prev_dict[diff],i]
+    #         prev_dict[n] = i
+    #     return
+
+        my_dict ={}
+        for i,v in enumerate(nums):
+            diff = target - v
+            if diff in my_dict:
+                return [my_dict[diff],i]
+            my_dict[v] = i    
         return
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return i,j
