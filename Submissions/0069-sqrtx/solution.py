bc544679a035cd22c64f3eb44e09198871414a54
Bharath Shakthivel <75258943+BharathShakthivel@users.noreply.github.com>
2026-01-12 21:38:26 +0000

    [LeetCode Sync] Runtime - 8 ms (22.50%), Memory - 19.4 MB (35.25%)

--- Submissions/0069-sqrtx/solution.py
+++ Submissions/0069-sqrtx/solution.py
@@ -1,20 +1,34 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        if x == 0:
-            return 0
+        # if x == 0:
+        #     return 0
     
-        left = 1
-        right = x
+        # left = 1
+        # right = x
     
-        while left <= right:
-            mid = left + (right - left) // 2
+        # while left <= right:
+        #     mid = left + (right - left) // 2
         
-            if mid == x // mid:
-                return mid
-            elif mid < x // mid:
-                left = mid + 1
-            else:
-                right = mid - 1
+        #     if mid == x // mid:
+        #         return mid
+        #     elif mid < x // mid:
+        #         left = mid + 1
+        #     else:
+        #         right = mid - 1
     
     # When loop ends, right will be the floor of sqrt(x)
-        return right
+        # return right
+        l = 0
+        r = x
+        res = 0
+        while(l<=r):
+            m = l + ((r-l)//2)
+            if m ** 2 > x:
+                r = m -1
+            elif m ** 2 < x:
+                l = m + 1
+                res= m
+            else:
+                return m
+        return res
+
