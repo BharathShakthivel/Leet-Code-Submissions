4702bea8b429c850e0a3e5d16dec70d07912b361
Bharath Shakthivel <75258943+BharathShakthivel@users.noreply.github.com>
2026-01-14 23:02:44 +0000

    [LeetCode Sync] Runtime - 7 ms (81.25%), Memory - 20.8 MB (20.10%)

--- Submissions/0125-valid-palindrome/solution.py
+++ Submissions/0125-valid-palindrome/solution.py
@@ -21,9 +21,31 @@ class Solution:
         #     return False
         # return True
 
+
         # More Polish
+        # import re
+        # result = (re.sub(r'[^a-zA-Z0-9]', '', s)).lower()
+        # if result != result[::-1]:
+        #     return False
+        # return True
+
+        # “I first normalize the string by removing non-alphanumeric characters and converting everything to lowercase. Then I check whether the cleaned string is equal to its reverse. If it is, it’s a palindrome.”
+
+        # import re
+        # s = (re.sub(r'[^a-zA-Z0-9]', '', s)).lower()
+        # if s != s[::-1]:
+        #     return False
+        # return True
+
+        # Two pointer Approach
         import re
-        result = (re.sub(r'[^a-zA-Z0-9]', '', s)).lower()
-        if result != result[::-1]:
-            return False
+        s = (re.sub(r'[^a-zA-Z0-9]', '', s)).lower()
+        l = 0
+        r = len(s)-1
+        while l < r:
+            if s[l] == s[r]:
+                l+=1
+                r-=1
+            else:
+                return False
         return True
