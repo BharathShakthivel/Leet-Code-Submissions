32de07eb9774904602419c20c2ef39b2f67cde33
Bharath Shakthivel <75258943+BharathShakthivel@users.noreply.github.com>
2026-01-17 20:48:28 +0000

    [LeetCode Sync] Runtime - 7 ms (13.04%), Memory - 20.6 MB (51.68%)

--- Submissions/0026-remove-duplicates-from-sorted-array/solution.py
+++ Submissions/0026-remove-duplicates-from-sorted-array/solution.py
@@ -1,13 +1,39 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        write = 0
-        read = 1
-        while(read<=len(nums)-1):
-             if nums[read]!=nums[write]:
-                write+=1
-                nums[write]=nums[read]
-             else:
-                pass
-             read+=1
-        return write+1
+        # write = 0
+        # read = 1
+        # while(read<=len(nums)-1):
+        #      if nums[read]!=nums[write]:
+        #         write+=1
+        #         nums[write]=nums[read]
+        #      else:
+        #         pass
+        #      read+=1
+        # return write+1
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        write_pointer = 0
+        read_pointer = 1
+        while(read_pointer<len(nums)):
+            if nums[read_pointer]!= nums[write_pointer]:
+                write_pointer+=1
+                nums[write_pointer] = nums[read_pointer]
+            else:
+                pass
+            read_pointer+=1
+        return write_pointer+1
