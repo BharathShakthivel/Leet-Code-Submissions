29b1f61a869a91348b5ccdcf38286db4c1a1e51b
Bharath Shakthivel <75258943+BharathShakthivel@users.noreply.github.com>
2026-01-20 23:21:15 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 20 MB (55.36%)

--- Submissions/0035-search-insert-position/solution.py
+++ Submissions/0035-search-insert-position/solution.py
@@ -1,8 +1,20 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        for index,value in enumerate(nums):
-            if value == target:
-                return index
-            elif value > target:
-                return index
-        return len(nums)
+        # for index,value in enumerate(nums):
+        #     if value == target:
+        #         return index
+        #     elif value > target:
+        #         return index
+        # return len(nums)
+        
+        l,h = 0 , len(nums)-1
+        while l <=h:
+            mid = l + ((h-l) // 2)
+            mid_num = nums[mid]
+            if mid_num == target:
+                return mid
+            elif mid_num > target:
+                h = mid - 1
+            elif mid_num < target:
+                l = mid + 1
+        return l
