378379017aaafaf5398b8188c4e73e965923d58c
Bharath Shakthivel <75258943+BharathShakthivel@users.noreply.github.com>
2026-01-21 18:40:09 +0000

    [LeetCode Sync] Runtime - 11 ms (84.50%), Memory - 22 MB (82.48%)

--- Submissions/0049-group-anagrams/solution.py
+++ Submissions/0049-group-anagrams/solution.py
@@ -1,19 +1,19 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        freq_dict = defaultdict(list)   
-        for s in strs:
-            str_count = [0] * 26
-            for c in s:
-                str_count[ord(c) - ord('a')] += 1
-            key = tuple(str_count)
-            freq_dict[key].append(s)
+        # freq_dict = defaultdict(list)   
+        # for s in strs:
+        #     str_count = [0] * 26
+        #     for c in s:
+        #         str_count[ord(c) - ord('a')] += 1
+        #     key = tuple(str_count)
+        #     freq_dict[key].append(s)
         
 
-        return list(freq_dict.values())
+        # return list(freq_dict.values())
         
-        #Dealing with soring method
-        # ans = defaultdict(list)
-        # for s in strs:
-        #     key = "".join(sorted(s))
-        #     ans[key].append(s)
-        # return list(ans.values())
+        # Dealing with soring method
+        ans = defaultdict(list)
+        for s in strs:
+            key = "".join(sorted(s))
+            ans[key].append(s)
+        return list(ans.values())
