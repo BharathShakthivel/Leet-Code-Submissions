885416f364e8240a4936019c480fc786a9c6f469
Bharath Shakthivel <75258943+BharathShakthivel@users.noreply.github.com>
2026-02-04 18:50:47 +0000

    [LeetCode Sync] Runtime - 44 ms (94.37%), Memory - 29.8 MB (20.26%)

---
+++ Submissions/0011-container-with-most-water/solution.py
@@ -0,0 +1,37 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        # Brute Force O(N2)
+        # res = 0
+        # for i in range(len(height)):
+        #     for j in range(i+1,len(height)):
+        #         if height[i]<height[j]:
+        #             cur_max = height[i] * (j-i)
+        #         else:
+        #             cur_max = height[j] * (j-i)
+        #         if cur_max>res:
+        #             res=cur_max
+        # return res
+
+
+        # Brute Force Simplified
+        # res = 0
+        # for i in range(len(height)):
+        #     for j in range(i+1,len(height)):
+        #         cur_max = min(height[i],height[j]) * (j-i)
+        #         if cur_max>res:
+        #             res=cur_max
+        # return res
+
+        # Optimal Solution
+
+        l,r = 0,len(height)-1
+        res = 0
+        while l<r:
+             cur_max = min(height[l],height[r]) * (r-l)
+             if cur_max>res:
+                res=cur_max
+             if height[l]<height[r]:
+                l+=1
+             else:
+                r-=1
+        return res
