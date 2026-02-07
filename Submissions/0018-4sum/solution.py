485c6d1ce24299cbff9861aba7bb34f739046d18
Bharath Shakthivel <75258943+BharathShakthivel@users.noreply.github.com>
2026-02-07 21:29:40 +0000

    [LeetCode Sync] Runtime - 383 ms (73.87%), Memory - 19.7 MB (8.40%)

---
+++ Submissions/0018-4sum/solution.py
@@ -0,0 +1,46 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        # FAILED ATTEMPT
+        
+        # nums.sort()
+        # res = []
+        # l,r = 0,len(nums)-1
+        # while l<r and l+1 !=r-1:
+        #         if nums[l] != nums[l+1] and nums[l] != nums[r] and nums[l]!= nums[r-1]:
+        #             four_sum = nums[l] + nums[l+1] + nums[r] + nums[r-1]
+        #             if four_sum>target:
+        #                 r-=1
+        #             elif four_sum<target:
+        #                 l+=1
+        #             else:
+        #                 res.append([nums[l],nums[l+1],nums[r],nums[r-1]])
+        #                 l+=1
+        #                 r-=1         
+        # return res
+        
+        # TWO POINTER - O(N3)
+        nums.sort()
+        res = []
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            for j in range(i+1,len(nums)):
+                if j > i+1 and nums[j] == nums[j-1]:
+                    continue
+                l,r = j+1,len(nums)-1
+                while l<r:
+                    four_sum = nums[i] + nums[j] + nums[l] + nums[r]
+                    if four_sum > target:
+                        r-=1
+                    elif four_sum < target:
+                        l+=1
+                    else:
+                        res.append([nums[i],nums[j],nums[l],nums[r]])
+                        l+=1
+                        r-=1
+                        while l<r and nums[l] == nums[l-1]:
+                            l+=1
+                        while r>l and nums[r] == nums[r+1]:
+                            r-=1
+        return res
+
