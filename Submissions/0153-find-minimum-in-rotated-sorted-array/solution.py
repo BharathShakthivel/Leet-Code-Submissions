da50b985eaf731ff550b91818dfdba1ed928c6f8
Bharath Shakthivel <75258943+BharathShakthivel@users.noreply.github.com>
2026-02-11 20:12:22 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.5 MB (37.10%)

---
+++ Submissions/0153-find-minimum-in-rotated-sorted-array/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        #Brute Force - Time - O(n), Space - O(1)
+        # nums.sort()
+        # return nums[0]
+        # Min index Method 
+        l,r = 0, len(nums)-1
+        while l<r:
+            m = (l+r)//2
+            if nums[m]>nums[r]:
+                l = m+1
+            else:
+                r = m
+
+        if nums[0]<nums[l]:
+            return nums[0]
+        return nums[l] 
