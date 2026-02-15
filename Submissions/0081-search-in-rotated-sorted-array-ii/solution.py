18824e2530baec86e88f988271cda0c4fbcfc0b0
Bharath Shakthivel <75258943+BharathShakthivel@users.noreply.github.com>
2026-02-15 22:24:23 +0000

    [LeetCode Sync] Runtime - 4 ms (2.84%), Memory - 19.6 MB (41.61%)

--- Submissions/0081-search-in-rotated-sorted-array-ii/solution.py
+++ Submissions/0081-search-in-rotated-sorted-array-ii/solution.py
@@ -11,18 +11,22 @@ class Solution:
             mid = l + ((r-l)//2)
             if nums[mid] == target:
                 return True
-            # Checking if Target belongs to the left Portion:
+            # LEFT PORTION IS SORTED
             if nums[l]<nums[mid]:
+                # Checking if Target belongs to the left Portion:
                 if nums[l]<=target<nums[mid]:
                     r = mid-1
                 else:
                     l = mid+1
+            # RIGHT PORTION IS SORTED
             elif nums[l]>nums[mid]:
+                # Checking if Target belongs to the right Portion:
                 if nums[r]>= target > nums[mid]:
                     l = mid+1
                 else:
                     r = mid-1
             else:
+            # At this point whole of the values between left index and mid index contains same value and duplicates so we just shift one pointer.
                 l+=1
         return False
 
