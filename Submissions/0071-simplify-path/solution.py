f694bea14c012fe6d6dd8aebda1da021a432b379
Bharath Shakthivel <75258943+BharathShakthivel@users.noreply.github.com>
2026-02-24 22:54:24 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.2 MB (71.66%)

--- Submissions/0071-simplify-path/solution.py
+++ Submissions/0071-simplify-path/solution.py
@@ -1,5 +1,25 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
+        
+        """
+        # 1. Split the path by "/"
+        # 2. Create an empty stack
+        # 3. For each part:
+        #       - If "" or "."  → ignore
+        #       - If ".."       → pop from stack (if not empty)
+        #       - Else          → push folder name to stack
+        # 4. Join stack with "/" and add leading "/"
+        # 5. Return result
+
+        """
+        
+
+
+
+
+
+        
+        
         stack = []
         x = path.split("/")
         for i in x:
