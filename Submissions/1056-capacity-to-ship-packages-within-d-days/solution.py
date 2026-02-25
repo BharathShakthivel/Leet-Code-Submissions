e4c9514a86d9319dd6abf03c11e25fc4f165aa67
Bharath Shakthivel <75258943+BharathShakthivel@users.noreply.github.com>
2026-02-25 22:49:15 +0000

    [LeetCode Sync] Runtime - 311 ms (16.01%), Memory - 23.9 MB (39.51%)

---
+++ Submissions/1056-capacity-to-ship-packages-within-d-days/solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        l = max(weights)
+        r =sum(weights)
+        valid_capacity = []
+        while l<=r:
+            mid = (l+r)//2
+            cur_sum = 0
+            cur_day = 1
+            for i in range(len(weights)):
+                if cur_sum + weights[i]>mid:
+                    cur_day+=1
+                    cur_sum = weights[i]
+                else:
+                    cur_sum+=weights[i]
+            if cur_day>days:
+                l = mid+1
+            elif cur_day<=days:
+                r = mid-1
+                valid_capacity.append(mid)
+        return min(valid_capacity)
+        
+
+
