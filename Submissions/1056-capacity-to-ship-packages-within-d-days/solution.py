06ece707da462a354b2161a76270c2fe1ec0749d
Bharath Shakthivel <75258943+BharathShakthivel@users.noreply.github.com>
2026-02-27 22:32:54 +0000

    [LeetCode Sync] Runtime - 607 ms (5.07%), Memory - 24.1 MB (16.74%)

--- Submissions/1056-capacity-to-ship-packages-within-d-days/solution.py
+++ Submissions/1056-capacity-to-ship-packages-within-d-days/solution.py
@@ -19,6 +19,27 @@ class Solution:
                 r = mid-1
                 valid_capacity.append(mid)
         return min(valid_capacity)
+
+
+        #Optimised Version 
+        # We know that once we shrink the weights, the minimum would be left pointer value that corresponds to minimum capacity of the ship to load all the goods in order in d days.
+        l = max(weights)
+        r =sum(weights)
+        while l<=r:
+            mid = (l+r)//2
+            cur_sum = 0
+            cur_day = 1
+            for i in range(len(weights)):
+                if cur_sum + weights[i]>mid:
+                    cur_day+=1
+                    cur_sum = weights[i]
+                else:
+                    cur_sum+=weights[i]
+            if cur_day>days:
+                l = mid+1
+            elif cur_day<=days:
+                r = mid
+        return l
         
 
 
