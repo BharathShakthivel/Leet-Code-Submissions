f09f3a8512045ab0dcf017c36b14f6c2d4477c01
Bharath Shakthivel <75258943+BharathShakthivel@users.noreply.github.com>
2026-03-02 12:23:08 +0000

    [LeetCode Sync] Runtime - 31 ms (34.83%), Memory - 27.6 MB (21.97%)

--- Submissions/0238-product-of-array-except-self/solution.py
+++ Submissions/0238-product-of-array-except-self/solution.py
@@ -2,6 +2,39 @@
 # import math
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        prefix = [1] * n
+        suffix = [1] * n
+        answer = [1] * n
+        for i in range(1,n):
+            prefix[i] = prefix[i-1] * nums[i-1]
+        for j in range(n-2,-1,-1):
+            suffix[j] = suffix[j+1] * nums[j+1]
+        for k in range(n):
+            answer[k] = prefix[k] * suffix[k]
+        return answer
+
+        # right_product = 1
+        # for j in range(n-1,-1,-1):
+        #     prefix[j] = prefix[j] * right_product
+        #     right_product = right_product * nums[j]
+        # return prefix
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         # answer = []
         # for i in range(len(nums)):
         #     cur_prefix = nums[0:i]
@@ -25,12 +58,12 @@ class Solution:
 
 # Most Optimal Solution - Time - O(N) ; Space = O(1)
 
-        n = len(nums)
-        prefix = [1] * n
-        for i in range(1,n):
-            prefix[i] = nums[i-1] * prefix[i-1]
-        right_product = 1
-        for j in range(n-1,-1,-1):    
-            prefix[j] = prefix[j] * right_product
-            right_product*=nums[j]
-        return prefix
+        # n = len(nums)
+        # prefix = [1] * n
+        # for i in range(1,n):
+        #     prefix[i] = nums[i-1] * prefix[i-1]
+        # right_product = 1
+        # for j in range(n-1,-1,-1):    
+        #     prefix[j] = prefix[j] * right_product
+        #     right_product*=nums[j]
+        # return prefix
