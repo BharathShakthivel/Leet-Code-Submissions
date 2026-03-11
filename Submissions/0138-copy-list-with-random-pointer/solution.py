6dd66dd9fc2ef23736ff96ce224a2650d70eea22
Bharath Shakthivel <75258943+BharathShakthivel@users.noreply.github.com>
2026-03-11 21:05:42 +0000

    [LeetCode Sync] Runtime - 55 ms (13.70%), Memory - 20.1 MB (66.15%)

---
+++ Submissions/0138-copy-list-with-random-pointer/solution.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        # Two Pass Algorithm
+        old_copy = {None:None} # To handle edge case of current pointer pointing to NULL when traversing.
+        cur = head
+        while cur:
+            old_copy[cur] = Node(cur.val)
+            cur = cur.next
+        cur = head
+        while cur:
+            copy = old_copy[cur]
+            copy.next = old_copy[cur.next]
+            copy.random = old_copy[cur.random]
+            cur=cur.next
+        return old_copy[head]
