3aed61cb44e00194a88bfc3d7d8db8938c67587b
Bharath Shakthivel <75258943+BharathShakthivel@users.noreply.github.com>
2026-03-14 20:35:51 +0000

    [LeetCode Sync] Runtime - 1 ms (72.95%), Memory - 20.2 MB (88.94%)

--- Submissions/0122-best-time-to-buy-and-sell-stock-ii/solution.py
+++ Submissions/0122-best-time-to-buy-and-sell-stock-ii/solution.py
@@ -1,5 +1,6 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        # O(N) Time and Space Complexity
         profit = 0
         for i in range(1,len(prices)):
             if prices[i]>prices[i-1]:
