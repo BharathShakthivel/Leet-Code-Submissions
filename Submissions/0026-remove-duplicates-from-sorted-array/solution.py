1f22af4276bd1395fb233f52c21bbc0230bf7dc0
Bharath Shakthivel <75258943+BharathShakthivel@users.noreply.github.com>
2026-03-30 22:43:40 +0000

    [LeetCode Sync] Runtime - 1 ms (55.73%), Memory - 20.6 MB (51.68%)

--- Submissions/0026-remove-duplicates-from-sorted-array/solution.py
+++ Submissions/0026-remove-duplicates-from-sorted-array/solution.py
@@ -11,29 +11,25 @@ class Solution:
         #      read+=1
         # return write+1
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        write_pointer = 0
-        read_pointer = 1
-        while(read_pointer<len(nums)):
-            if nums[read_pointer]!= nums[write_pointer]:
-                write_pointer+=1
-                nums[write_pointer] = nums[read_pointer]
+        l = 1
+        r = 1
+        while r != len(nums):
+            if nums[r] != nums[r-1]:
+                nums[l] = nums[r]
+                l+=1
+                r+=1
             else:
-                pass
-            read_pointer+=1
-        return write_pointer+1
+                r+=1
+        return l
+
+
+        # write_pointer = 0
+        # read_pointer = 1
+        # while(read_pointer<len(nums)):
+        #     if nums[read_pointer]!= nums[write_pointer]:
+        #         write_pointer+=1
+        #         nums[write_pointer] = nums[read_pointer]
+        #     else:
+        #         pass
+        #     read_pointer+=1
+        # return write_pointer+1
