f775d59efa54611aa8d6c72be94cbfbeb6c825f0
Bharath Shakthivel <75258943+BharathShakthivel@users.noreply.github.com>
2026-04-08 04:42:51 +0000

    [LeetCode Sync] Runtime - 115 ms (36.19%), Memory - 61.3 MB (59.79%)

--- Submissions/0077-combinations/solution.py
+++ Submissions/0077-combinations/solution.py
@@ -1,15 +1,17 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
+        
+        # Time k * n!/(n-k)! * k!   ; Space: O(N)
         res,sol = [],[]
         def backtrack(i):
             if len(sol) == k:
-                res.append(sol[:])
+                res.append(sol[:])  #Taking a copy of the solution
                 return
-            for j in range(i,n+1):
+            for j in range(i,n+1): # We go all the way till n + 1 and including N
                 sol.append(j)
                 backtrack(j+1)
                 sol.pop()
-        backtrack(1)
+        backtrack(1) # We start from 1 as it is mentioned in the problem
         return res
 
             
