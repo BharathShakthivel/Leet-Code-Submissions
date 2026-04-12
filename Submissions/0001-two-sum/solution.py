3396dc98e996a77b1f86e3bb9bcf70c936badc52
Bharath Shakthivel <75258943+BharathShakthivel@users.noreply.github.com>
2026-04-12 19:59:15 +0000

    [LeetCode Sync] Runtime - 3 ms (52.87%), Memory - 20.4 MB (47.53%)

--- Submissions/0001-two-sum/solution.py
+++ Submissions/0001-two-sum/solution.py
@@ -1,12 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        prev_dict = {}
-        for i,n in enumerate(nums):
-            diff = target - n
-            if diff in prev_dict:
-                return [prev_dict[diff],i]
-            prev_dict[n] = i
-        return
+#         prev_dict = {}
+#         for i,n in enumerate(nums):
+#             diff = target - n
+#             if diff in prev_dict:
+#                 return [prev_dict[diff],i]
+#             prev_dict[n] = i
+#         return
 
         # my_dict ={}
         # for i,v in enumerate(nums):
@@ -19,5 +19,66 @@ class Solution:
         #     for j in range(i+1,len(nums)):
         #         if nums[i] + nums[j] == target:
         #             return i,j
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # Binary Search Index Mismatch
+
+        # nums.sort()
+        # for i in range(len(nums)):
+
+        #         diff = target - nums[i]
+        #         l,r = i+1, len(nums)-1
+        #         while l<=r:
+        #             mid = (l+r)//2
+        #             if nums[mid] > diff:
+        #                 r = mid -1
+        #             elif nums[mid] < diff:
+        #                 l = mid +1
+        #             else:
+        #                 return i,mid
+
+        # Optimal
+
+        my_dict = {}
+        for i,v in enumerate(nums):
+            diff = target - v
+            if diff in my_dict.keys():
+                return [i,my_dict[diff]]
+            my_dict[v] = i
+
+
+
+
+
+
+
+
+
+
+
+
 
 
