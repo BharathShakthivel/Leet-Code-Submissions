004335fb56b1d998dff2c1eb9c7c38c4a4e0caa7
Bharath Shakthivel <75258943+BharathShakthivel@users.noreply.github.com>
2026-05-06 22:04:01 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 20.6 MB (8.55%)

--- Submissions/0001-two-sum/solution.py
+++ Submissions/0001-two-sum/solution.py
@@ -8,17 +8,17 @@ class Solution:
 #             prev_dict[n] = i
 #         return
 
-        # my_dict ={}
-        # for i,v in enumerate(nums):
-        #     diff = target - v
-        #     if diff in my_dict:
-        #         return [my_dict[diff],i]
-        #     my_dict[v] = i    
-        # return
-        # for i in range(len(nums)):
-        #     for j in range(i+1,len(nums)):
-        #         if nums[i] + nums[j] == target:
-        #             return i,j
+        my_dict ={}
+        for i,v in enumerate(nums):
+            diff = target - v
+            if diff in my_dict:
+                return [my_dict[diff],i]
+            my_dict[v] = i    
+        return
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] == target:
+                    return i,j
 
 
 
