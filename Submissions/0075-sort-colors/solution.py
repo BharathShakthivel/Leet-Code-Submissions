ff049bef1de135b3d1959da27d50542458f5e419
Bharath Shakthivel <75258943+BharathShakthivel@users.noreply.github.com>
2026-07-27 18:13:52 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.2 MB (63.35%)

--- Submissions/0075-sort-colors/solution.py
+++ Submissions/0075-sort-colors/solution.py
@@ -3,18 +3,62 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        '''
+        Dutch National Flag Algorithm - Three pointer
+        If 0 → swap with low, low++, mid++
+        If 1 → mid++
+        If 2 → swap with high, high--
+
+        Here mid is looping from start till it crosses right pointer
+        '''
+        # l,r = 0, len(nums)-1
+        # i = 0
+        # def swap(i,j):
+        #     temp = nums[i]
+        #     nums[i] = nums[j]
+        #     nums[j] = temp
+        # while i<=r:
+        #     if nums[i] == 0:
+        #         swap(l,i)
+        #         l+=1
+        #     elif nums[i] == 2:
+        #         swap(i,r)
+        #         r-=1
+        #         i-=1
+        #     i+=1
+        # return nums
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         l,r = 0, len(nums)-1
         i = 0
-        def swap(i,j):
-            temp = nums[i]
-            nums[i] = nums[j]
-            nums[j] = temp
-        while i<=r:
-            if nums[i] == 0:
+        def swap (a,b):
+            temp = nums[a]
+            nums[a] = nums [b]
+            nums[b] = temp
+        while i <=r:
+            if nums[i]==0:
                 swap(l,i)
                 l+=1
-            elif nums[i] == 2:
-                swap(i,r)
+            elif nums[i]==2:
+                swap(r,i)
                 r-=1
                 i-=1
             i+=1
