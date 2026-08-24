0fb0ed510b5d36340eef41c308d7b1aa4ba5c273
Bharath Shakthivel <75258943+BharathShakthivel@users.noreply.github.com>
2026-08-24 20:56:12 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (54.20%)

--- Submissions/0070-climbing-stairs/solution.py
+++ Submissions/0070-climbing-stairs/solution.py
@@ -1,8 +1,9 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        #  Basically adding the ways to reach the target through Bottom up Approach 
         one,two = 1,1
         for i in range(n-1):
             temp = one
-            one = one + two
+            one= one + two
             two = temp
         return one
