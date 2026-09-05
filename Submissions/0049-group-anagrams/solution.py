f2d9eab13c0c65adbe41d8ff38b18a5c61567829
Bharath Shakthivel <75258943+BharathShakthivel@users.noreply.github.com>
2026-09-05 13:39:49 +0000

    [LeetCode Sync] Runtime - 15 ms (41.24%), Memory - 21.9 MB (87.94%)

--- Submissions/0049-group-anagrams/solution.py
+++ Submissions/0049-group-anagrams/solution.py
@@ -3,7 +3,7 @@ class Solution:
         # freq_dict = defaultdict(list)   
         # for s in strs:
         #     str_count = [0] * 26
-        #     for c in s:
+        #     for c in s.lower():
         #         str_count[ord(c) - ord('a')] += 1
         #     key = tuple(str_count)
         #     freq_dict[key].append(s)
@@ -11,36 +11,27 @@ class Solution:
 
         # return list(freq_dict.values())
         
-        # # Dealing with sorting method
-        # ans = defaultdict(list)
-        # for s in strs:
-        #     key = "".join(sorted(s))
-        #     ans[key].append(s)
-        # return list(ans.values())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # Practise
-        my_dict = defaultdict(list)
+        # Dealing with sorting method
+        ans = defaultdict(list)
         for s in strs:
-            key = [0] * 26
-            for i in s:
-                key[ord("a")-ord(i)]+=1
-            my_dict[tuple(key)].append(s)
-        return list(my_dict.values())
+            key = "".join(sorted(s))
+            ans[key].append(s)
+        return list(ans.values())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
